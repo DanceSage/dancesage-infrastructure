de07be94f3e34e9b99d1c1091152ea8f986a62b5
Abdu Radi <92768651+AbduKhRadi@users.noreply.github.com>
2025-12-21 19:03:38 -0500

    adding the variables file

---
+++ environments/dev/main.tf
@@ -0,0 +1,10 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+  project  = var.project_id
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  enable_autopilot = false
+}
